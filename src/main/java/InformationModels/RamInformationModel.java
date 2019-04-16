8362e7598a8dfaa23b2ac86b5af1d9dbe2a7ae10
Kakaranish <stasiekgruz@gmail.com>
2019-04-16 20:51:22 +0200

    Added description to class RamInformationModel.

--- src/main/java/InformationModels/RamInformationModel.java
+++ src/main/java/InformationModels/RamInformationModel.java
@@ -9,6 +9,11 @@ public class RamInformationModel implements IInformationModel
     public String Buffers;
     public String Cached;
 
+    /*
+        commandExecutionResult looks like:
+        Mem:       2011984     215400    1796584          0      88300      69316
+    */
+
     public RamInformationModel(String commandExecutionResult)
     {
         commandExecutionResult = commandExecutionResult.trim();
