0b6adedcd1b561c889d4144af15e7b8c31a33171
Kakaranish <stasiekgruz@gmail.com>
2019-04-27 20:23:43 +0200

    Used to Encrypter class signleton design pattern.

--- src/main/java/Encrypter.java
+++ src/main/java/Encrypter.java
@@ -1,5 +1,8 @@
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 public class Encrypter
@@ -7,26 +10,73 @@ public class Encrypter
    private Cipher _ecipher;
    private Cipher _decipher;
 
-   public Encrypter(SecretKey key) throws Exception
-   {
-       _ecipher = Cipher.getInstance("AES");
-       _decipher = Cipher.getInstance("AES");
+   private static final Encrypter _encrypter;
 
-       _ecipher.init(Cipher.ENCRYPT_MODE, key);
-       _decipher.init(Cipher.DECRYPT_MODE, key);
+    static
+    {
+        Encrypter encrypter = null;
+        try
+        {
+            String key = AppProperties.GetInstance().Properties.getProperty("SecretKey");
+            SecretKey secretKey = new SecretKeySpec(key.getBytes(), "AES");
+            encrypter = new Encrypter(secretKey);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            _encrypter = encrypter;
+        }
+    }
+
+   private Encrypter(SecretKey key) throws EncrypterException
+   {
+       try
+       {
+           _ecipher = Cipher.getInstance("AES");
+           _decipher = Cipher.getInstance("AES");
+
+           _ecipher.init(Cipher.ENCRYPT_MODE, key);
+           _decipher.init(Cipher.DECRYPT_MODE, key);
+       }
+       catch (NoSuchAlgorithmException|NoSuchPaddingException|InvalidKeyException e)
+       {
+           throw new EncrypterException("Initialization of Encrypter failed");
+       }
    }
 
-   public String encrypt(String str) throws Exception
+   public static Encrypter GetInstance()
    {
-       byte[] utf8 = str.getBytes("UTF8");
-       byte[] enc = _ecipher.doFinal(utf8);
-       return Base64.getEncoder().encodeToString(enc);
+       return _encrypter;
    }
 
-   public String decrypt(String str) throws Exception
+   public String Encrypt(String str) throws EncrypterException
    {
-       byte[] dec = Base64.getDecoder().decode(str);
-       byte[] utf8 = _decipher.doFinal(dec);
-       return new String(utf8, "UTF8");
+       try
+       {
+           byte[] utf8 = str.getBytes("UTF8");
+           byte[] enc = _ecipher.doFinal(utf8);
+           return Base64.getEncoder().encodeToString(enc);
+       }
+       catch (BadPaddingException|UnsupportedEncodingException|IllegalBlockSizeException e)
+       {
+           throw new EncrypterException("Encryption failed.");
+       }
+   }
+
+   public String Decrypt(String str) throws EncrypterException
+   {
+       try
+       {
+           byte[] dec = Base64.getDecoder().decode(str);
+           byte[] utf8 = _decipher.doFinal(dec);
+           return new String(utf8, "UTF8");
+       }
+       catch (BadPaddingException|UnsupportedEncodingException|IllegalBlockSizeException e)
+       {
+           throw new EncrypterException("Decryption failed.");
+       }
    }
 }
