1bf55c72937852cfe3d0ac78c23729ffb33b0d64
Kakaranish <stasiekgruz@gmail.com>
2019-04-28 18:44:30 +0200

    Added LogsManager which is responsible for: - loading computers and matching to them preferences from Db - loading list of all avaiable preferences - starting gathering logs for ComputerManagers

---
+++ src/main/java/LogsManager.java
@@ -0,0 +1,134 @@
+import Entities.Computer;
+import Entities.ComputerPreference;
+import Entities.Preference;
+import Preferences.IPreference;
+import org.hibernate.Session;
+
+import javax.persistence.Query;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class LogsManager
+{
+    public final int NumOfRetries =
+            Integer.parseInt(AppProperties.GetInstance().Properties.getProperty("NumOfRetries"));
+    public final int Cooldown =
+            Integer.parseInt(AppProperties.GetInstance().Properties.getProperty("Cooldown"));
+
+
+    private List<ComputerManager> _computerManagers;
+    private List<Preference> _availablePreferences;
+
+
+    public LogsManager()
+            throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException,
+            InstantiationException, IllegalAccessException
+    {
+        _availablePreferences = GetAvailablePreferencesFromDb();
+        _computerManagers = GetComputerManagersFromDb();
+
+        // Create&Run MaintainManager
+
+        // Run logs gathering for all computers
+        for (ComputerManager computerManager : _computerManagers)
+        {
+            StartGatheringLogsForComputerManager(computerManager);
+        }
+
+        try
+        {
+            Thread.sleep(30000);
+        } catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+//        _computerManagers.get(0).SetAsNotGathering();
+    }
+
+    private List<Preference> GetAvailablePreferencesFromDb()
+    {
+        Session session = DatabaseManager.GetInstance().GetSession();
+        session.beginTransaction();
+
+        String hql = "from Preference";
+        Query query = session.createQuery(hql);
+        List<Preference> preferences = query.getResultList();
+
+        session.close();
+        return preferences;
+    }
+
+    private List<ComputerManager> GetComputerManagersFromDb()
+            throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException,
+            InstantiationException, IllegalAccessException
+    {
+        List<ComputerManager> computerManagers = new ArrayList<>();
+
+        Map<Computer, List<ComputerPreference>> grouped =
+                LoadComputerPreferencesFromDb().stream().collect(Collectors.groupingBy(cp -> cp.Computer));
+        for (Map.Entry<Computer, List<ComputerPreference>> computerListEntry : grouped.entrySet())
+        {
+            Computer computer = computerListEntry.getKey();
+            List<IPreference> preferences = new ArrayList<>();
+            for (ComputerPreference computerPreference : computerListEntry.getValue())
+            {
+                Preference preferenceEntity = computerPreference.Preference;
+                IPreference preference = ConvertPreferenceEntityToIPreference(preferenceEntity);
+                preferences.add(preference);
+            }
+
+            computerManagers.add(new ComputerManager(this, computer, preferences));
+        }
+
+        return computerManagers;
+    }
+
+    private List<ComputerPreference> LoadComputerPreferencesFromDb()
+    {
+        Session session = null;
+        session = DatabaseManager.GetInstance().GetSession();
+        session.beginTransaction();
+
+        String hql = "from ComputerPreference compPref";
+        Query query = session.createQuery(hql);
+        List<ComputerPreference> preferences = query.getResultList();
+
+        session.close();
+        return  preferences;
+    }
+
+    private IPreference ConvertPreferenceEntityToIPreference(Preference preference)
+            throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException,
+            InvocationTargetException, InstantiationException
+    {
+        String iPreferenceClassName = preference.ClassName;
+        Class iPreferenceClass = Class.forName(iPreferenceClassName);
+        IPreference iPreference = (IPreference) iPreferenceClass.getConstructor().newInstance();
+        return iPreference;
+    }
+
+    private void StartGatheringLogsForComputerManager(ComputerManager computerManager)
+    {
+        computerManager.start();
+    }
+
+    public void GatheringStoppedCallback(ComputerManager computerManager)
+    {
+        System.out.println("Stopped");
+    }
+
+
+    public void GatheringSSHConnectionErrorCallback(ComputerManager computerManager)
+    {
+
+    }
+
+    public void GatheringErrorCallback(ComputerManager computerManager)
+    {
+        System.out.println("Error");
+        _computerManagers.remove(computerManager);
+    }
+}
