02a3c8912cc5fd9b4cc59905772c3024d22234b4
Kakaranish <stasiekgruz@gmail.com>
2019-04-28 18:36:46 +0200

    Got rid of typos.

---
+++ src/main/java/Preferences/UsersInfoPreference.java
@@ -0,0 +1,17 @@
+package Preferences;
+
+import Models.Info.IInfo;
+import Models.Info.UsersInfo;
+
+public class UsersInfoPreference implements IPreference
+{
+    public String GetCommandToExecute()
+    {
+        return "w";
+    }
+
+    public IInfo GetInformationModel(String commandExecutionResult)
+    {
+        return new UsersInfo(commandExecutionResult);
+    }
+}
