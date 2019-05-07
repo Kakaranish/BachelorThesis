b631b869f5f9efaf0e66ff6c924a540733c029f2
Kakaranish <stasiekgruz@gmail.com>
2019-05-07 19:19:59 +0200

    Added copy constructor to Computer class which is used in UpdateComputer in ComputerManager.

--- src/main/java/Computer.java
+++ src/main/java/Computer.java
@@ -1,16 +1,32 @@
 import Entities.ComputerEntity;
 import Preferences.IPreference;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer
 {
     public ComputerEntity ComputerEntity; // Needed encapsulation of computer class
     public List<IPreference> ComputerPreferences;
-
+    
     public Computer(Entities.ComputerEntity computerEntity, List<IPreference> computerPreferences)
     {
         ComputerEntity = computerEntity;
         ComputerPreferences = computerPreferences;
+    }
+
+    public Computer(Computer computer)
+    {
+        ComputerEntity = new ComputerEntity(computer.ComputerEntity);
+
+        if(ComputerPreferences == null)
+        {
+            ComputerPreferences = new ArrayList<>();
+        }
+
+        for (IPreference computerPreference : computer.ComputerPreferences)
+        {
+            ComputerPreferences.add(computerPreference);
+        }
     }
 }
