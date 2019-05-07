6fbd57c3c616c0dbaaea986a6b58747aeab7c879
Kakaranish <stasiekgruz@gmail.com>
2019-05-07 19:27:01 +0200

    Added skeleton of logs maintainer - class responsible for maintaing logs.

---
+++ src/main/java/LogsMaintainer.java
@@ -0,0 +1,15 @@
+import Entities.ComputerEntity;
+import org.hibernate.Session;
+
+public class LogsMaintainer
+{
+    public LogsMaintainer()
+    {
+
+    }
+
+    public void RemoveLogsAssociatedWithComputerFromDb(Computer computer, Session session)
+    {
+
+    }
+}
