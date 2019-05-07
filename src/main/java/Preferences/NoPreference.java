e27bba5b7e523fd4369702989438970618c4c932
Kakaranish <stasiekgruz@gmail.com>
2019-05-07 19:25:52 +0200

    Implemented NoPreference class.

--- src/main/java/Preferences/NoPreference.java
+++ src/main/java/Preferences/NoPreference.java
@@ -1,5 +1,16 @@
 package Preferences;
 
-public class NoPreference
+import Models.Info.IInfo;
+
+public class NoPreference implements IPreference
 {
+    public String GetCommandToExecute()
+    {
+        return null;
+    }
+
+    public IInfo GetInformationModel(String commandExecutionResult)
+    {
+        return null;
+    }
 }
