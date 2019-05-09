20f0ccd2c9296c336b62b85f05be0f88ce66234f
Kakaranish <stasiekgruz@gmail.com>
2019-05-09 23:19:53 +0200

    Added class Classroom responsible for containing classroom name and description.

---
+++ src/main/java/Entities/Classroom.java
@@ -0,0 +1,29 @@
+package Entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Classrooms")
+public class Classroom
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer Id;
+
+    @Column(unique = true, nullable = false)
+    public String Name;
+
+    public String Description;
+
+    private Classroom()
+    {
+    }
+
+    public Classroom(String name, String description)
+    {
+        Id = null;
+        Name = name;
+        Description = description;
+    }
+}
+
