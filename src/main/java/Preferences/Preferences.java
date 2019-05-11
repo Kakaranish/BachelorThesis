2b62ae0e6c8ed70049ca26de4c955bd9ae09c838
Kakaranish <stasiekgruz@gmail.com>
2019-05-12 00:44:11 +0200

    Added Preferences class which holds IPreference classes because instanting them everytime we need them is useless. Instances are always the same.

--- src/main/java/Preferences/Preferences.java
+++ src/main/java/Preferences/Preferences.java
@@ -2,4 +2,17 @@ package Preferences;
 
 public class Preferences
 {
+    public static final CpuInfoPreference CpuInfoPreference = new CpuInfoPreference();
+
+    public static final DisksInfoPreference DisksInfoPreference = new DisksInfoPreference();
+
+    public static final NoPreference NoPreference = new NoPreference();
+
+    public static final ProcessesInfoPreference ProcessesInfoPreference = new ProcessesInfoPreference();
+
+    public static final RamInfoPreference RamInfoPreference = new RamInfoPreference();
+
+    public static final SwapInfoPreference SwapInfoPreference = new SwapInfoPreference();
+
+    public static final UsersInfoPreference UsersInfoPreference = new UsersInfoPreference();
 }
