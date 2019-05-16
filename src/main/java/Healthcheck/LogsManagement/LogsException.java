4a95cedbdfb79419515b39bb9adfcfaeb8d516fa
Kakaranish <stasiekgruz@gmail.com>
2019-05-16 22:42:37 +0200

    Renamed class related with log exception.

---
+++ src/main/java/Healthcheck/LogsManagement/LogsException.java
@@ -0,0 +1,9 @@
+package Healthcheck.LogsManagement;
+
+public class LogsException extends Exception
+{
+    public LogsException(String s)
+    {
+        super(s);
+    }
+}
--- src/main/java/Healthcheck/LogsManagement/LogsMaintainerException.java
+++
@@ -1,9 +0,0 @@
-package Healthcheck.LogsManagement;
-
-public class LogsMaintainerException extends Exception
-{
-    public LogsMaintainerException(String s)
-    {
-        super(s);
-    }
-}
