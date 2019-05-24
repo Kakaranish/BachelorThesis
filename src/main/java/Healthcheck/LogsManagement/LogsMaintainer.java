b0e8f18df3a81d30a0271a8445985e1e4cfb91fc
Kakaranish <stasiekgruz@gmail.com>
2019-05-24 21:05:09 +0200

    Finally fixed issue in LogsMaintainer when collision occurs and last maintenance update cannot be applied.

--- src/main/java/Healthcheck/LogsManagement/LogsMaintainer.java
+++ src/main/java/Healthcheck/LogsManagement/LogsMaintainer.java
@@ -74,7 +74,6 @@ public class LogsMaintainer extends Thread
             for (Computer computer : _logsManager.GetConnectedComputers())
             {
                 long computerTimeToMaintenance = GetComputerTimeToMaintenance(computer);
-                System.out.println("time to maintain: " + computerTimeToMaintenance);
 
                 if( computerWithLowestTimeToMaintain == null ||
                         computerTimeToMaintenance < computerWithLowestTimeToMaintain.TimeToMaintain)
@@ -122,10 +121,11 @@ public class LogsMaintainer extends Thread
         }
     }
 
-    public void MaintainComputer(Computer computer)
+    public boolean MaintainComputer(Computer computer)
     {
-        System.out.println("INSIDE MAINTAINER");
         long logExpiration = computer.ComputerEntity.LogExpiration.toMillis();
+        String attemptErrorMessage = "[ERROR] LogsMaintainer: Attempt of deleting logs for '"
+                + computer.ComputerEntity.Host + "' failed.";
 
         for (IPreference computerPreference : computer.Preferences)
         {
@@ -136,125 +136,35 @@ public class LogsMaintainer extends Thread
                     "and (" + now  + " - t.Timestamp) > " + logExpiration;
 
             Session session = DatabaseManager.GetInstance().GetSession();
-            Query query = null;
-
-            query = session.createQuery(hql);
+            Query query = session.createQuery(hql);
             query.setParameter("computerEntity", computer.ComputerEntity);
 
-
-            System.out.println("AFTER QUERY");
             boolean removingLogsSucceed =
-                    ExecuteQueryWithRetryPolicy(session, query, _logsManager.GetComputerLoggerForComputer(computer));
+                    DatabaseManager.ExecuteDeleteQueryWithRetryPolicy(session, query, attemptErrorMessage);
             if (removingLogsSucceed == false)
             {
-                System.out.println("REMOVING LOGS == FALSE");
+                String host = computer.ComputerEntity.Host;
+                System.out.println("[FATAL ERROR] LogsMaintainer: Deleting logs for '" + host + "' failed.");
+
                 session.close();
-                return;
+                return false;
             }
 
             session.close();
         }
-        System.out.println("OUTSIDE MAINTAINING1");
         Timestamp nowTimestamp = new Timestamp(System.currentTimeMillis());
         try
         {
             _logsManager.SetComputerLastMaintenance(computer, nowTimestamp);
         }
-        catch (Exception e)
+        catch (DatabaseException e)
         {
-            e.printStackTrace(System.out);
-        }
-        System.out.println("OUTSIDE MAINTAINING2");
-    }
-
-    private boolean ExecuteQueryWithRetryPolicy(Session session, Query query, ComputerLogger computerLogger)
-    {
-        try
-        {
-            session.beginTransaction();
-            System.out.println("BEFORE EXECUTE");
-            query.executeUpdate();
-            System.out.println("AFTER EXECUTE");
-            session.getTransaction().commit();
-            System.out.println("AFTER COMMIT");
-
-            return true;
-        }
-        catch (Exception e)
-        {
-            System.out.println("[ERROR] '"
-                    + computerLogger.GetComputer().ComputerEntity.Host
-                    + "': LogsMaintainer - executing query attempt failed. Database is locked.");
-
-            e.printStackTrace(System.out);
-            session.getTransaction().rollback();
-
-            int retryNum = 1;
-            while(retryNum <= Utilities.LogSaveNumOfRetries)
-            {
-                try
-                {
-                    Thread.sleep(Utilities.LogSaveRetryCooldown
-                            + new Random().ints(0,45).findFirst().getAsInt());
-
-                    session.beginTransaction();
-                    query.executeUpdate();
-                    session.getTransaction().commit();
-
-                    return true;
-                }
-                catch (InterruptedException ex)
-                {
-                    _logsManager.Callback_Maintainer_ComputerLoggerThreadSleepInterrupted(computerLogger);
-                    return false;
-                }
-                catch (Exception ex)
-                {
-                    session.getTransaction().rollback();
-                    ++retryNum;
-
-                    System.out.println("[ERROR] '"
-                            + computerLogger.GetComputer().ComputerEntity.Host
-                            + "': LogsMaintainer - query attempt failed. Database is locked.");
-                }
-            }
-
-            _logsManager.Callback_Maintainer_ExecuteQueryFailedAfterRetries(computerLogger);
             return false;
         }
+
+        return true;
     }
 
-    // TODO: Add retry policy
-    public static void RemoveAllLogsAssociatedWithComputers(Computer computer) throws DatabaseException
-    {
-        Session session = DatabaseManager.GetInstance().GetSession();
-
-        try
-        {
-            session.beginTransaction();
-
-            for (IPreference computerPreference : Preferences.AllPreferencesList)
-            {
-                String hql = "delete from " +
-                        computerPreference.GetClassName() +
-                        " t where t.ComputerEntity = :computerEntity";
-                Query query = session.createQuery(hql);
-                query.setParameter("computerEntity", computer.ComputerEntity);
-
-                query.executeUpdate();
-            }
-
-            session.getTransaction().commit();
-        }
-        catch (PersistenceException e)
-        {
-            throw new DatabaseException("[FATAL ERROR] Unable te remove all logs associated with computer");
-        }
-        finally
-        {
-            session.close();
-        }
-    }
 
     public static void RemoveAllLogsAssociatedWithComputerFromDb(Computer computer, Session session)
     {
