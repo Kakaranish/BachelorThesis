05c049a7662a0ad5588900177c50eae135b401b0
Kakaranish <stasiekgruz@gmail.com>
2019-06-07 23:03:00 +0200

    Removed unsuned reference.

--- src/main/java/Healthcheck/DatabaseManagement/DatabaseManager.java
+++ src/main/java/Healthcheck/DatabaseManagement/DatabaseManager.java
@@ -1,7 +1,6 @@
 package Healthcheck.DatabaseManagement;
 
 import Healthcheck.Utilities;
-import jdk.jshell.execution.Util;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
